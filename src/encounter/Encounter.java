a69fa2ca0f9683298a9bdda5e5618f39a0aaeda7
Jonathan <jonathan.kohlhas@gmx.de>
2017-01-13 18:57:49 +0100

    stuff

--- src/encounter/Encounter.java
+++ src/encounter/Encounter.java
@@ -14,6 +14,13 @@ import world.World;
  */
 public class Encounter
 {
+
+    private final int BUTTON_X = 50;
+    private final int BUTTON_Y = 50;
+    private final int BUTTON_WIDTH = 100;
+    private final int BUTTON_HEIGHT = 18;
+    private final int BUTTON_DIFF = 20;
+
     private World world;
 
 	private String text;
@@ -28,18 +35,23 @@ public class Encounter
 
 	public int chance;
 
+	private State state;
+
+
+	private enum State{
+	    STARTED,RESULT,INACTIVE
+	}
+
 	class solutionButton implements ButtonCall{
         private Solution solution;
-        private Encounter encounter;
 
-        public solutionButton(Solution solution,Encounter encounter){
+        public solutionButton(Solution solution){
             this.solution = solution;
-            this.encounter = encounter;
         }
 
         @Override
         public void call() {
-            showResult(solution.resolve(world));
+            Encounter.this.showResult(solution.resolve(world));
         }
     }
 
@@ -48,20 +60,27 @@ public class Encounter
 	    this.world = world;
 	    this.text = text;
 	    this.solutions = solutions;
+	    this.state = State.INACTIVE;
+
+	    for(int x = 0; x < solutions.length;x++){
+            buttons[x] = new Button(BUTTON_X,BUTTON_Y+ x * BUTTON_DIFF , BUTTON_WIDTH,BUTTON_HEIGHT,solutions[x].getText(),new solutionButton(solutions[x]));
+        }
 	}
 
 	public void startEncounter()
 	{
+	    this.state = State.STARTED;
 	}
 
 	public void showResult(String[] result)
     {
         this.results = result;
+        this.state = State.RESULT;
     }
 
     public void endEncounter()
     {
-
+        this.state = State.INACTIVE;
     }
 
 
