7b45306b37185926154f9d902ae894c62ab32fee
Jonathan <jonathan.kohlhas@gmx.de>
2017-01-13 15:18:21 +0100

    Display and Input management added

--- src/Game/Display.java
+++ src/Game/Display.java
@@ -1,7 +1,37 @@
 package Game;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowListener;
+
 /**
  * Created by jonathan on 13.01.17.
  */
-public class Display {
+public class Display extends JFrame {
+
+    /**
+     * Constructs a Display
+     * @param width width of the Display
+     * @param height height of the Display
+     */
+    public Display(int width, int height){
+        super("Sailing");
+        setSize(width,height);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addMouseListener(InputManager.getInstance());
+        addKeyListener(InputManager.getInstance());
+        addWindowListener(InputManager.getInstance());
+        pack();
+        setIgnoreRepaint(true);
+        createBufferStrategy(2);
+    }
+
+    public Graphics getBackBuffer(){
+        return getBufferStrategy().getDrawGraphics();
+    }
+
+    public void flipBuffers(){
+        getBufferStrategy().show();
+    }
 }
---
+++ src/Game/InputManager.java
@@ -0,0 +1,120 @@
+package Game;
+
+import javax.swing.event.MouseInputListener;
+import java.awt.event.*;
+
+/**
+ * Created by jonathan on 13.01.17.
+ */
+public class InputManager implements MouseInputListener,KeyListener,WindowListener{
+
+    private static InputManager inputManager;
+
+    private InputManager(){
+
+    }
+
+    public static InputManager getInstance(){
+        if(inputManager == null){
+            inputManager = new InputManager();
+        }
+        return inputManager;
+    }
+
+    /*
+     * Keyboard
+     */
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    /*
+     * Mouse
+     */
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    /*
+     * Window
+     */
+
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+
+    }
+}
