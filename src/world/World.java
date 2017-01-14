6be08d2df33aea4b67446ff5e8b8ee8deddc927b
Chillmaster <niklas.jspeckels@gmail.com>
2017-01-14 15:15:06 +0100

    Stuff

--- src/world/World.java
+++ src/world/World.java
@@ -14,105 +14,113 @@ import player.Player.Condition;
  */
 public class World
 {
-    private static final int NO_ENCOUNTER_CHANCE = 60;
+	private static final int NO_ENCOUNTER_CHANCE = 60;
 
-    public Player player;
+	public Player player;
 
-    int size;
+	int size;
 
-    Condition currentCondition = player.condition;
+	Condition currentCondition = player.condition;
 
-    Encounter[] encounters;
+	Encounter[] encounters;
 
-    private LinkedList<Encounter> worldEncounters;
+	private LinkedList<Encounter> worldEncounters;
 
-    private LinkedList<Encounter> activeEncounters;
+	private LinkedList<Encounter> activeEncounters;
 
-    public World(Player player, int size)
-    {
+	public World(Player player, int size)
+	{
 
-        this.player = player;
-        this.size = size;
-        activeEncounters = new LinkedList<>();
-        worldEncounters = new LinkedList<>();
+		this.player = player;
+		this.size = size;
+		activeEncounters = new LinkedList<>();
+		worldEncounters = new LinkedList<>();
 
+		for (int i = 5; i < size - 5; i++)
+		{
+			int random = (int) (Math.random() * (sumOfEncounterChance(encounters) + NO_ENCOUNTER_CHANCE) + 1)
+					- NO_ENCOUNTER_CHANCE;
+			if (random > 0) worldEncounters.add(encounterFactory(random, i));
+		}
 
-        for (int i = 5; i < size - 5; i++)
-        {
-            int random = (int) (Math.random() * (sumOfEncounterChance(encounters) + NO_ENCOUNTER_CHANCE) + 1) - NO_ENCOUNTER_CHANCE;
-            if (random > 0)
-                worldEncounters.add(encounterFactory(random, i));
-        }
+		Collections.sort(worldEncounters, Encounter.getComparator());
 
-    }
+		for (Encounter encounter : worldEncounters)
+		{
+			removeCooldownEvents(encounter);
+		}
 
-    public void draw(Graphics g)
-    {
-        if (activeEncounters.size() > 0) activeEncounters.get(activeEncounters.size() - 1).draw(g);
-    }
+	}
 
-    public void update()
-    {
+	public void draw(Graphics g)
+	{
+		if (activeEncounters.size() > 0) activeEncounters.get(activeEncounters.size() - 1).draw(g);
+	}
 
-    }
+	public void update()
+	{
 
+	}
 
-    public void addActiveEncounter(Encounter encounter)
-    {
-        activeEncounters.add(encounter);
-    }
+	public void addActiveEncounter(Encounter encounter)
+	{
+		activeEncounters.add(encounter);
+	}
 
-    public void removeActiveEncounter(Encounter encounter)
-    {
-        activeEncounters.remove(encounter);
-    }
+	public void removeActiveEncounter(Encounter encounter)
+	{
+		activeEncounters.remove(encounter);
+	}
 
-    public void addWorldEncounter(Encounter encounter)
-    {
-        worldEncounters.add(encounter);
-        Collections.sort(worldEncounters, Encounter.getComparator());
-        removeCooldownEvents(encounter);
-    }
+	public void addWorldEncounter(Encounter encounter)
+	{
+		worldEncounters.add(encounter);
+		Collections.sort(worldEncounters, Encounter.getComparator());
+		removeCooldownEvents(encounter);
+	}
 
-    public void removeWorldEncounter(Encounter encounter)
-    {
-        worldEncounters.remove(encounter);
-    }
+	public void removeWorldEncounter(Encounter encounter)
+	{
+		worldEncounters.remove(encounter);
+	}
 
-    public int sumOfEncounterChance(Encounter[] encounters)
-    {
-        int total = 0;
+	public int sumOfEncounterChance(Encounter[] encounters)
+	{
+		int total = 0;
 
-        for (Encounter encounter : encounters)
-        {
-            total += encounter.chance;
-        }
+		for (Encounter encounter : encounters)
+		{
+			total += encounter.chance;
+		}
 
-        return total;
-    }
+		return total;
+	}
 
-    private void removeCooldownEvents(Encounter encounter)
-    {
-        for (Encounter encounter1 : worldEncounters)
-        {
-            if (encounter.distance <= encounter1.distance && encounter.distance + encounter.cooldown >= encounter1.distance && encounter1.priority <= encounter.priority)
-            {
-                worldEncounters.remove(encounter1);
-            } else if (encounter1.distance > encounter.distance + encounter.cooldown) break;
-        }
-    }
+	private void removeCooldownEvents(Encounter encounter)
+	{
+		for (Encounter encounter1 : worldEncounters)
+		{
+			if (encounter.distance <= encounter1.distance
+					&& encounter.distance + encounter.cooldown >= encounter1.distance
+					&& encounter1.priority <= encounter.priority)
+			{
+				worldEncounters.remove(encounter1);
+			} else if (encounter1.distance > encounter.distance + encounter.cooldown) break;
+		}
+	}
 
-    private Encounter encounterFactory(int listPosition, int worldDistance)
-    {
-        for (Encounter encounter : encounters)
-        {
-            listPosition -= encounter.chance;
-            if (listPosition < 0)
-            {
-                return new Encounter(this, encounter.text, encounter.solutions, 0, worldDistance, encounter.priority, encounter.cooldown);
-            }
-        }
-        return null;
-    }
+	private Encounter encounterFactory(int listPosition, int worldDistance)
+	{
+		for (Encounter encounter : encounters)
+		{
+			listPosition -= encounter.chance;
+			if (listPosition < 0)
+			{
+				return new Encounter(this, encounter.text, encounter.solutions, 0, worldDistance, encounter.priority,
+						encounter.cooldown);
+			}
+		}
+		return null;
+	}
 
 }
